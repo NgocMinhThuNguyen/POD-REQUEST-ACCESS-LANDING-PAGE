6a8b1dd7c661bc14347793e1afa97e719ad18388
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2023-11-07 13:33:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-const userEmail = input.value;
 
-console.log(form);
-console.log('hi');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  validateForm();
+});
+
+function validateForm() {
+  const userEmail = input.value;
+  let error = input.nextElementSibling;
+  
+    if (userEmail === '') {
+      showError(input, error);
+    } else {
+      validateEmail(userEmail, error);
+    }
+  }
+
+
+function validateEmail(userEmail, error) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!(emailRegex.test(userEmail))) {
+    showError(input, error);
+  } else {
+    showSucces(input, error);
+  }
+}
+
+function showError(input, error) {
+  input.classList.add("show-error");
+  error.classList.add("show-error");
+}
+
+function showSucces(input, error) {
+  input.classList.remove("show-error");
+  error.classList.remove("show-error");
+}
